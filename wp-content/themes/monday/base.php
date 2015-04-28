db69b82d91d31b3f4a9d97104472d06c170523a6
Aaron Frey <aaron.frey@gmail.com>
2015-04-28 10:32:14 -0400

    Fix footer

--- wp-content/themes/monday/base.php
+++ wp-content/themes/monday/base.php
@@ -6,42 +6,29 @@ use Roots\Sage\Wrapper;
 ?>
 
 <?php get_template_part('templates/head'); ?>
+
   <body <?php body_class(); ?>>
-    <!--[if lt IE 9]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-      </div>
-    <![endif]-->
+
     <?php
       do_action('get_header');
       get_template_part('templates/header');
     ?>
+
     <div class="wrap container-fluid" role="document">
       <div class="content row">
-        <main role="main">
-          <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (Config\display_sidebar()) : ?>
-          <aside class="sidebar" role="complementary">
-            <?php include Wrapper\sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
-        <?php endif; ?>
+        <?php include Wrapper\template_path(); ?>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
-    <?php
-      get_template_part('templates/footer');
-      wp_footer();
-    ?>
 
     <script src="<?php bloginfo('template_url'); ?>/assets/scripts/jquery.easing.1.3.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/assets/scripts/jquery.animate-enhanced.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/assets/scripts/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
     <script>
-      // if($('#slides img').length > 1) {
+      if($('#slides img').length > 1) {
         $('#slides').superslides({
           animation: 'fade'
         });
-      // }
+      }
     </script>
 
   </body>
--- wp-content/themes/monday/page-tenants.php
+++ wp-content/themes/monday/page-tenants.php
@@ -1,12 +1,19 @@
 <?php while (have_posts()) : the_post(); ?>
 
+	<div id="slides">
+		<ul class="slides-container">    
+			<li><img src="<?php bloginfo('template_url'); ?>/dist/images/tenants.jpg"></li>
+			<li><img src="<?php bloginfo('template_url'); ?>/dist/images/tenants.jpg"></li>
+		</ul>
+	</div>
+
 	<div class="mainwhite" align="center">
 
 		<div class="maincontent">
 
 			<div class="container">
 				
-				<div class="row no-gutter">
+				<div class="row">
 					
 					<div class="col-md-12">
 						
@@ -17,27 +24,29 @@
 
 				</div>
 
-				<div class="row no-gutter tenantrow">
+				<div class="row tenantrow">
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
-						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1000wilson.jpg" width="47" height="63">
-						<div>1000 Wilson Boulevard</div>
-						<a href="http://www.1000wilsonblvd.info/toc.cfm" target="_blank"> > TENANT HANDBOOK</a>
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
+						<div class="inner-tenant">
+							<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1000wilson.jpg" width="47" height="63">
+							<div>1000 Wilson Boulevard</div>
+							<a href="http://www.1000wilsonblvd.info/toc.cfm" target="_blank"> > TENANT HANDBOOK</a>
+						</div>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1100wilson.jpg" width="47" height="63">
 						<div>1100 Wilson Boulevard</div>
 						<a href="http://www.1100wilsonblvd.info/toc.cfm?CFID=635595&CFTOKEN=82001888" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1101wilson.jpg" width="47" height="63">
 						<div>1101 Wilson Boulevard</div>
 						<a href="http://www.1101wilsonblvd.info/toc.cfm?CFID=635733&CFTOKEN=76766645" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1200wilson.jpg" width="47" height="63">
 						<div>1200 Wilson Boulevard</div>
 						<a href="http://www.1200wilsonblvd.info/coming_soon.cfm" target="_blank"> > TENANT HANDBOOK</a>
@@ -45,27 +54,27 @@
 				
 				</div>
 
-				<div class="row no-gutter tenantrow">
+				<div class="row tenantrow">
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1400key.jpg" width="47" height="63">
 						<div>1400 Key Boulevard</div>
 						<a href="http://www.1400keyblvd.info/toc.cfm?CFID=635834&CFTOKEN=10435528" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1401wilson.jpg" width="47" height="63">
 						<div>1401 Wilson Boulevard</div>
 						<a href="http://www.1401wilsonblvd.info/toc.cfm?CFID=635967&CFTOKEN=74123853" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1501wilson.jpg" width="47" height="63">
 						<div>1501 Wilson Boulevard</div>
 						<a href="http://www.1501wilsonblvd.info/toc.cfm?CFID=635887&CFTOKEN=75283438" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1515wilson.jpg" width="47" height="63">
 						<div>1515 Wilson Boulevard</div>
 						<a href="http://www.1515wilsonblvd.info/toc.cfm?CFID=635960&CFTOKEN=31901447" target="_blank"> > TENANT HANDBOOK</a>
@@ -73,21 +82,21 @@
 
 				</div>
 
-				<div class="row no-gutter tenantrow">
+				<div class="row tenantrow">
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1701northfort.jpg" width="47" height="63">
 						<div>1701 North Fort Myer Drive</div>
 						<a href="http://www.1701nfortmyerdr.info/toc.cfm?CFID=636041&CFTOKEN=32830598" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/1812north.jpg" width="47" height="63">
 						<div>1812 North Moore</div>
 						<a href="http://1812northmoore.com/#firstPage" target="_blank"> > TENANT HANDBOOK</a>
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-md-3 tenant">
+					<div class="col-xs-12 col-sm-3 col-md-3 tenant">
 						<img src="<?php bloginfo('template_url'); ?>/dist/images/thumbs/21002nd.jpg" width="47" height="63">
 						<div>2100 2nd Street</div>
 						<a href="http://21002ndstreetsw.info/toc.cfm" target="_blank"> > TENANT HANDBOOK</a>
@@ -99,13 +108,13 @@
 
 		</div>
 
+		<?php
+		  get_template_part('templates/footer');
+		  wp_footer();
+		?>
+
 	</div>
 
-	<div id="slides">
-		<ul class="slides-container">    
-			<li><img src="<?php bloginfo('template_url'); ?>/dist/images/tenants.jpg" class="img-responsive"></li>
-			<li><img src="<?php bloginfo('template_url'); ?>/dist/images/tenants.jpg" class="img-responsive"></li>
-		</ul>
-	</div>
+
 
 <?php endwhile; ?>
