c773fff5bb825520ea5803083c866fe913c07bf9
Aaron Frey <aaron.frey@gmail.com>
2015-05-06 17:17:14 -0400

    Add home page link to mobile nav

--- wp-content/themes/monday/templates/header.php
+++ wp-content/themes/monday/templates/header.php
@@ -17,7 +17,9 @@
     <nav class="collapse navbar-collapse" role="navigation">
 
       <div class="visible-xs hide-sm mobile-navbar-collpase">
-        <img src="<?php bloginfo('template_url'); ?>/dist/images/logo2.png">
+        <a href="<?= esc_url(home_url('/')); ?>">
+          <img src="<?php bloginfo('template_url'); ?>/dist/images/logo2.png">
+        </a>
         <a href="#" class="mobile-nav-close" id="mobile-nav-close">
           <i class="fa fa-3x fa-close"></i>
         </a>
