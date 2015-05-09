70824cf0819846df3dc1eb516ea65462afc270e5
Aaron Frey <aaron.frey@gmail.com>
2015-05-08 23:35:00 -0400

    Fix javascript issues

--- wp-content/themes/monday/assets/scripts/main.js
+++ wp-content/themes/monday/assets/scripts/main.js
@@ -16,123 +16,66 @@
 
 (function($) {
 
-    // Use this variable to set up the common and page specific functions. If you
-    // rename this variable, you will also need to rename the namespace below.
-    var Sage = {
-        // All pages
-        'common': {
-            init: function() {
-                // JavaScript to be fired on all pages
-            },
-            finalize: function() {
-                $(document).on('click', '#mobile-nav-close', function() {
-                    $('.navbar-collapse').collapse('hide');
-                });
-                // JavaScript to be fired on all pages, after page specific JS is fired
-                $('.fancybox.iframe').on('click', function(e) {
-                    e.preventDefault();
-                    $.fancybox.open({
-                        href: window.home_url + '/investors.html',
-                        type: 'iframe'
-                    });
-                });
-            }
-        },
-        // Home page
-        'home': {
-            init: function() {
-                // JavaScript to be fired on the home page
-            },
-            finalize: function() {
-                // JavaScript to be fired on the home page, after the init JS
-                $('#slides').superslides({
-                    animation: 'fade'
-                });
-            }
-        },
-        'case_study': {
-            init: function() {
+    $(document).on('click', '#mobile-nav-close', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    // JavaScript to be fired on all pages, after page specific JS is fired
+    $('.fancybox.iframe').on('click', function(e) {
+        e.preventDefault();
+        $.fancybox.open({
+            href: window.home_url + '/investors.html',
+            type: 'iframe'
+        });
+    });
 
-                function equalCols() {
-                    if ($(window).width() > 479) {
-                        $('.case-image').height($('.case-description-col').height());
-                        $('.image-pagination').css('top', 0);
-                    } else {
-                        $('.case-image').height('auto');
-                        $('.image-pagination').css('top', $('.case-image img').height());
-                    }
-                }
+    $('#slides').superslides({
+        animation: 'fade'
+    });
 
-                equalCols();
-
-                $(window).resize(function() {
-                    equalCols();
-                    $('.ngg-casestudy').height($('.case-study.active').height());
-                });
-
-                var caseStudy = $('.ngg-casestudy .case-study').first();
-                var csHeight = $(caseStudy).height();
-                $('.ngg-casestudy').height(csHeight);
-                $(caseStudy).css('visibility', 'visible').addClass('active');
-
-                $('.image-pagination a').on('click', function(e) {
-                    e.preventDefault();
-                    $('.image-pagination a').removeClass('active');
-                    $(this).addClass('active');
-
-                    var idx = parseInt($(this).text()) - 1;
-                    caseStudy = $('.ngg-casestudy .case-study')
-                        .removeClass('active')
-                        .css('visibility', 'hidden')
-                        .eq(idx);
-                    var csHeight = $(caseStudy).height();
-
-                    $('.ngg-casestudy').height(csHeight);
-                    $(caseStudy).css('visibility', 'visible')
-                        .addClass('active')
-                        .find('img')
-                        .imageScale('scale');
-                });
-
-                $("img.scale").imageScale({
-                    fadeInDuration: 1000,
-                    rescaleOnResize: true
-                });
-            }
+    function equalCols() {
+        if ($(window).width() > 479) {
+            $('.case-image').height($('.case-description-col').height());
+            $('.image-pagination').css('top', 0);
+        } else {
+            $('.case-image').height('auto');
+            $('.image-pagination').css('top', $('.case-image img').height());
         }
-    };
+    }
 
-    // The routing fires all common scripts, followed by the page specific scripts.
-    // Add additional events for more control over timing e.g. a finalize event
-    var UTIL = {
-        fire: function(func, funcname, args) {
-            var fire;
-            var namespace = Sage;
-            funcname = (funcname === undefined) ? 'init' : funcname;
-            fire = func !== '';
-            fire = fire && namespace[func];
-            fire = fire && typeof namespace[func][funcname] === 'function';
+    equalCols();
 
-            if (fire) {
-                namespace[func][funcname](args);
-            }
-        },
-        loadEvents: function() {
-            // Fire common init JS
-            UTIL.fire('common');
+    $(window).resize(function() {
+        equalCols();
+        $('.ngg-casestudy').height($('.case-study.active').height());
+    });
 
-            // Fire page-specific init JS, and then finalize JS
-            $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-                UTIL.fire(classnm);
-                UTIL.fire(classnm, 'finalize');
-            });
+    var caseStudy = $('.ngg-casestudy .case-study').first();
+    var csHeight = $(caseStudy).height();
+    $('.ngg-casestudy').height(csHeight);
+    $(caseStudy).css('visibility', 'visible').addClass('active');
 
-            // Fire common finalize JS
-            UTIL.fire('common', 'finalize');
-        }
-    };
+    $('.image-pagination a').on('click', function(e) {
+        e.preventDefault();
+        $('.image-pagination a').removeClass('active');
+        $(this).addClass('active');
 
-    // Load Events
-    $(document).ready(UTIL.loadEvents);
+        var idx = parseInt($(this).text()) - 1;
+        caseStudy = $('.ngg-casestudy .case-study')
+            .removeClass('active')
+            .css('visibility', 'hidden')
+            .eq(idx);
+        var csHeight = $(caseStudy).height();
+
+        $('.ngg-casestudy').height(csHeight);
+        $(caseStudy).css('visibility', 'visible')
+            .addClass('active')
+            .find('img')
+            .imageScale('scale');
+    });
+
+    $("img.scale").imageScale({
+        fadeInDuration: 1000,
+        rescaleOnResize: true
+    });
 
 })(jQuery); // Fully reference jQuery after this point.
