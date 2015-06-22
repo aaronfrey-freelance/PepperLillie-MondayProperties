27a04d048a27b72ae3aec8bb9e8325af42a44d6f
Aaron Frey <aaron.frey@gmail.com>
2015-06-22 16:50:51 -0400

    Make a default page for all other added pages

--- wp-content/themes/monday/page.php
+++ wp-content/themes/monday/page.php
@@ -1,4 +1,35 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+
+	<?php 
+	if ( has_post_thumbnail() ) {
+		the_post_thumbnail('full', ['class' => 'img-responsive']);
+	} 
+	?>
+
+	<div class="mainwhite" align="center">
+
+		<div class="maincontent">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-md-12">
+						<?php get_template_part('templates/page', 'header'); ?>
+						<?php get_template_part('templates/content', 'page'); ?>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<?php
+		  get_template_part('templates/footer');
+		  wp_footer();
+		?>
+
+	</div>
+
 <?php endwhile; ?>
