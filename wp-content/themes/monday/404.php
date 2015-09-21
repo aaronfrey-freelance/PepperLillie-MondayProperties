5541fd4126a5d35b3c3495241704bd20166dd7a9
Aaron Frey <aaron.frey@gmail.com>
2015-09-21 15:44:13 -0400

    Style 404 page

--- wp-content/themes/monday/404.php
+++ wp-content/themes/monday/404.php
@@ -1,7 +1,17 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<div class="container text-center">
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+	<div class="row">
+
+		<div class="col-md-12">
+
+			<?php get_template_part('templates/page', 'header'); ?>
+
+			<div class="alert alert-warning">
+			  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+			</div>
+
+		</div>
+
+	</div>
+
 </div>
-
-<?php get_search_form(); ?>
