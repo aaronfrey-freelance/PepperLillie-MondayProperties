8ad4c8757db819ba5e9b946b542056716ac3805e
Aaron Frey <aaron.frey@gmail.com>
2016-01-28 17:04:17 -0500

    Fix footer

--- wp-content/themes/monday/templates/footer.php
+++ wp-content/themes/monday/templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="content-info" role="contentinfo">
 
 	<div class="footertext">
-		&copy; 2015 Monday Properties.  All Rights Reserved  &nbsp; | &nbsp;<a href="<?php bloginfo('url'); ?>/contact">Contact Us</a>
+		&copy; <?= date('Y'); ?> Monday Properties.  All Rights Reserved  &nbsp; | &nbsp;<a href="<?php bloginfo('url'); ?>/contact">Contact Us</a>
 		<span class="hidden-xs">&nbsp; | &nbsp;<a href="<?php bloginfo('url'); ?>/careers">Careers</a></span>
 	</div>
 
@@ -13,9 +13,9 @@
 	$items = wp_get_nav_menu_items($term->term_id);
 
 	foreach ($items as $key => $menuitem) : ?>
-		<a href="<?php echo $menuitem->url; ?>" target="_blank">
+		<!-- <a href="<?php echo $menuitem->url; ?>" target="_blank">
 			<i class="<?php echo implode(' ', $menuitem->classes) ?>"></i>
-		</a>
+		</a> -->
 	<?php endforeach; ?>
 
 	</div>
